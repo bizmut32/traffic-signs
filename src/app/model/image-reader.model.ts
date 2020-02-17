a82d7589456368fadd576720f5af6f0bf5790e2e
Adam Balassa <balassaadi@gmail.com>
2020-02-17 10:28:10 +0100

    app root fix

--- src/app/model/image-reader.model.ts
+++ src/app/model/image-reader.model.ts
@@ -3,8 +3,9 @@ import * as path from 'path';
 const base64Img = require('base64-img');
 
 export function readImageInBase64(fileName: string): Promise<string> {
+  const appRoot = require('app-root-path');
   return new Promise((resolve, reject) => {
-    const filePath = `${__dirname}/../../${fileName}`;
+    const filePath = `${appRoot.path}/src/${fileName}`;
     base64Img.base64(filePath, (err: any, data: string) => {
       if (err) return reject(err);
       resolve(data);
